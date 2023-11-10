b6229efed3e18dfa069477caf4125af4b6d82e9f
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-10 17:41:34 -0300

    mods-fixs

--- src/app/galeria/buconero/page.jsx
+++ src/app/galeria/buconero/page.jsx
@@ -10,13 +10,12 @@ import Clavijas from "../../../../public/galeria/buconero/clavijas.png";
 export default function Buconero() {
   return (
     <>
-      <section className=" lg:grid lg:col-span-2 lg:row-span-4 lg:gap-2">
-        <header className="my-2 flex justify-between items-center rounded-xl col-span-2">
-          <p className="bg-[#14181b] text-lg text-center px-6 py-4 rounded-lg lg:text-4xl row-start-1 row-end-2">
-            Buconero
+      <section className="gap-2 lg:grid lg:col-span-2 lg:row-span-4 lg:gap-2">
+        <header className="rounded-xl col-span-2">
+          <p className="bg-[#14181b] mt-2 text-4xl text-center py-2 rounded-full lg:text-4xl row-start-1 row-end-2">
+            BUCONERO
           </p>
-
-          <figure className="bg-[#14181b] hover:bg-[#20262b] text-xl text-center px-2 py-2 rounded-full col-start-3 col-end-5 row-start-1 row-end-2">
+          <figure className="flex justify-start place-content-center mt-2 bg-[#14181b] hover:bg-[#20262b] text-xl text-center px-2 py-2 rounded-full">
             <Image src={Info} height="auto" width="auto" alt="icono info" />
           </figure>
         </header>
--- src/app/galeria/layout.jsx
+++ src/app/galeria/layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({ children }) {
       <figure className="p-4 row-start-1 row-end-3 col-start-1 col-end-3 lg:row-start-1 lg:row-end-4 lg:col-start-1 lg:col-end-2 rounded-2xl">
         <Link
           href="/"
-          className="absolute top-1 left-1 lg:left-12 bg-[#14181b] hover:bg-[#20262b] text-xl text-center px-2 py-2 my-2 rounded-full"
+          className="absolute top-2 left-2 lg:top-6 lg:left-6 border border-slate-800 bg-[#14181b] hover:bg-[#20262b] text-xl text-center px-2 py-2 my-2 rounded-full"
         >
           <Image
             src={Volver}
