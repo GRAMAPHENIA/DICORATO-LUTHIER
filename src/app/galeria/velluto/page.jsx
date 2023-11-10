5b67803d4398bd4c8e571100f5c311f6bd3cdedb
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-10 13:57:34 -0300

    responsive

--- src/app/galeria/velluto/page.jsx
+++ src/app/galeria/velluto/page.jsx
@@ -8,10 +8,10 @@ import Clavijas from "../../../../public/galeria/clavijas.png";
 export default function Velluto() {
   return (
     <>
-      <header className="flex justify-between items-center px-8 rounded-xl bg-[#14181b] col-span-full ">
+      <header className="flex justify-between items-center px-2 rounded-xl bg-[#14181b] col-span-full ">
         <p className=" text-2xl lg:text-4xl">VELLUTO</p>
         <p className=" text-xl text-center px-4 py-3 my-4 border-2 border-[#393c40]  hover:bg-[#20262b] rounded-full">
-          detalles »
+         + info
         </p>
       </header>
       <article className="bg-[#14181b] flex justify-center rounded-xl col-start-1 col-end-3 row-start-2 row-end-4">
