d9cee6a4ea87f647602c4ecc00ab57f73e5b86ea
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-11 10:34:28 -0300

    mobile

--- src/app/galeria/buconero/page.jsx
+++ src/app/galeria/buconero/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import BuconeroHorz from "../../../../public/croquis/buconero-horz.png";
 import VellutoHorz from "../../../../public/croquis/velluto-horz.png";
@@ -11,32 +12,49 @@ import Presentacion from "@/components/Presentacion.jsx";
 
 export default function Buconero() {
   return (
-    <section className="grid mt-2 grid-cols-1 lg:grid lg:grid-cols-2 gap-2">
-      <aside className=" bg-[#14181b] rounded-2xl col-span-2 p-10">
+    <section className=" grid my-2 grid-cols-1 lg:grid lg:grid-cols-2 gap-2">
+      <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10">
         <Image
           src={BuconeroHorz}
           alt="Logo de dicorato luthier"
           width="auto"
           height="auto"
         />
-      </aside>
-      <aside className=" bg-[#14181b] rounded-2xl col-span-2 p-10">
+        <Link
+          href="/"
+          className="absolute bottom-2 right-2 lg:bottom-10 lg:right-10 detalles text-base lg:text-xl text-center w-[100px] lg:w-[200px] px-0 py-1 lg:px-4 lg:py-3 ml-8 border-2 border-[#393c40] hover:bg-[#20262b] rounded-full"
+        >
+          detalles
+        </Link>
+      </figure>
+      <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10">
         <Image
           src={VellutoHorz}
           alt="Logo de dicorato luthier"
           width="auto"
           height="auto"
         />
-      </aside>
-      <aside className=" bg-[#14181b] rounded-2xl col-span-2 p-10">
+         <Link
+          href="/"
+          className="absolute bottom-2 right-2 lg:bottom-10 lg:right-10 detalles text-base lg:text-xl text-center w-[100px] lg:w-[200px] px-0 py-1 lg:px-4 lg:py-3 ml-8 border-2 border-[#393c40] hover:bg-[#20262b] rounded-full"
+        >
+          detalles
+        </Link>
+      </figure>
+      <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-10">
         <Image
           src={EiraHorz}
           alt="Logo de dicorato luthier"
           width="auto"
           height="auto"
         />
-      </aside>
-     
+         <Link
+          href="/"
+          className="absolute bottom-2 right-2 lg:bottom-10 lg:right-10 detalles text-base lg:text-xl text-center w-[100px] lg:w-[200px] px-0 py-1 lg:px-4 lg:py-3 ml-8 border-2 border-[#393c40] hover:bg-[#20262b] rounded-full"
+        >
+          detalles
+        </Link>
+      </figure>
     </section>
   );
 }
