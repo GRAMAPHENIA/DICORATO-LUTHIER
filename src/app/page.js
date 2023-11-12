68bf1eb345b86ec3d02ab2e881223db15240572f
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-11 22:12:05 -0300

    lutz

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,12 @@ import BodyBuconero from "@/components/BodyBuconero.jsx";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="relative">
       <Header />
+      <div className="absolute bg-[#3a94d956] -z-10  h-72 w-96 top-[250px] left-[10px] blur-[150px] rounded-full"></div>
       <Body />
       <Marcas />
+      <div className="absolute bg-[#3a89d95f] -z-10  h-96 w-96 top-[900px] right-[10px] blur-[150px] rounded-full"></div>
       <BodyBuconero />
     </main>
   );
