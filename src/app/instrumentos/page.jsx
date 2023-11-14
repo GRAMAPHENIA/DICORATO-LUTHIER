7d014846baec394a2467bb8be81217a9c227d1bc
gramaphenia <dicoratojuanpablo@gmail.com>
2023-11-13 21:56:46 -0300

    fixs

--- src/app/instrumentos/page.jsx
+++ src/app/instrumentos/page.jsx
@@ -6,19 +6,12 @@ import VellutoHorz from "../../../public/croquis/velluto-horz.svg";
 import EiraHorz from "../../../public/croquis/eira-horz.svg";
 
 export const metadata = {
-  title: 'Instrumentos',
-}
+  title: "Instrumentos",
+};
 
 export default function Buconero() {
   return (
     <>
-      <section className="border-8 border-[#3b4f6323] my-2 rounded-3xl">
-        <div className="absolute top-[280px] lg:top-[220px] left-[34%] lg:left-[34%] h-[15px] w-[200px]  lg:h-[30px] lg:w-[500px] bg-[#61b0ffda] -z-10 rounded-full blur-[50px] lg:blur-[100px]"></div>
-        <h1 className="flex text-4xl lg:text-[100px] text-center leading-[50px] lg:leading-[75px] justify-center align-middle px-2 my-20 bg-clip-text text-transparent bg-gradient-to-r from-[#121518] via-[#637d97] to-[#ffffff] font-extralight tracking-tight">
-          MODELOS Y DETALLES
-        </h1>
-      </section>
-
       <section className="grid my-2 grid-cols-1 lg:grid lg:grid-cols-2 gap-2">
         <figure className="relative bg-[#14181b] rounded-2xl col-span-2 p-4">
           <p>Modelo Buconero</p>
